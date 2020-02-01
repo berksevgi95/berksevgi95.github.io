1bf5eb2a23b442a07e46285f6fe84931b44b9cc8
Berk Sevgi <berksevgi95@hotmail.com>
2020-02-01 18:45:47 +0300

    Github page

---
+++ cv/precache-manifest.79f8d05c4164056a8a5dbb6ebf0194f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1850b8f1eefd1555ebf86312d5ece116",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a87f1b1e81ca2d74d0d9",
+    "url": "./static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "f32c09161e24cfbe9c38",
+    "url": "./static/css/main.61255c17.chunk.css"
+  },
+  {
+    "revision": "a87f1b1e81ca2d74d0d9",
+    "url": "./static/js/2.fb395a68.chunk.js"
+  },
+  {
+    "revision": "1a2a50b2bb69fda58fa90be44b85cfe5",
+    "url": "./static/js/2.fb395a68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f32c09161e24cfbe9c38",
+    "url": "./static/js/main.93be79bb.chunk.js"
+  },
+  {
+    "revision": "210e015b0f4054115d9d",
+    "url": "./static/js/runtime-main.e53f40df.js"
+  }
+]);
